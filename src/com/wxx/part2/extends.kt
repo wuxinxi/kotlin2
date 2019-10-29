a36b5195232753a394e0d28d45d24ca8c90d13b5
TangRen <996489865@qq.com>
2019-10-29 21:48:34 +0800

    函数2

---
+++ src/com/wxx/part2/extends.kt
@@ -0,0 +1,18 @@
+package com.wxx.part2;
+
+/**
+ * @author TangRen
+ * @create 2019-10-29 21:39
+ * @TODO:  类的继承
+ * 类开放性的修饰符：
+ * 1. public 默认的修饰符
+ * 2. internal 只对本模块生效即app自身
+ * 3. protected 只对自己和子类开放
+ * 4. private 只对自己开放
+ * open不是开放性修饰符但是他能觉得这个类是否能够被继承派生子类，具有open的类才有资格被继承
+ * 注意open 与private不兼容
+ */
+
+fun main() {
+
+}
